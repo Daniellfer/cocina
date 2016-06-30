5715556ccf8757eaa15694125b6eb427452fdf34
Dafell <dafellfig@gmail.com>
2016-06-30 04:23:13 -0400

    routes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//importar express para la comunicación de la aplicación 
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 7000));
 
+//carpeta estática donde se van a ubicar los asets
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// directorio de las los templates de las vistas
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//directorio del archivo donde sestan las rutas
 require('./app/frontend/routes.js')(app);
 
 app.listen(app.get('port'), function() {
